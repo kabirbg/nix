35a312b5242a7d3a28cb99650f33a473f940d2bf
Kabir Gupta <kabirg108@gmail.com>
2023-08-16 21:43:00 -0400

    init commit

---
+++ configuration.nix
@@ -0,0 +1,109 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "xps";
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+
+  # Enable sound.
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;    
+    jack.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.kabir = {
+    home = "/home/kabir";
+    isNormalUser = true;
+    description = "Kabir Gupta";
+    extraGroups = [ "wheel" "audio" "video" "networkmanager" ];
+    packages = with pkgs; [
+      alacritty
+      dunst
+      libsForQt5.polkit-kde-agent
+      libsForQt5.qtwayland
+      qt6.qtwayland
+      thunderbird
+    ];
+    shell = pkgs.zsh;
+  };
+
+  programs = {
+    git.enable = true;
+    zsh.enable = true;
+    hyprland.enable = true;
+    hyprland.xwayland.hidpi = true;
+    firefox.enable = true;
+    firefox.package = pkgs.firefox-devedition-bin;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    neovim
+    helix
+  ];
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  hardware.opengl.extraPackages = with pkgs; [ intel-compute-runtime intel-media-driver ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
